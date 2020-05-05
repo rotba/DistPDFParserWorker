b492b9f59d736669eaf8560b241a2a83b877cd5e
rotba <rotba@post.bgu.ac.il>
2020-05-05 12:06:20 +0300

    maybe now

--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -1,7 +1,7 @@
 public class Utils {
     public static void waitParsingTime() {
         try {
-            Thread.sleep(25*1000);
+            Thread.sleep(35*1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
